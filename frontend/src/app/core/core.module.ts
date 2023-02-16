e10b1a51552730bb17b1652794958176115bb0f8
ytyrpych <tyrpuchy0611@gmail.com>
2023-02-16 20:35:32 +0200

    add: add footer add: add template of login page form

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { PageHeaderComponent } from '../shared/page-header/page-header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginFormComponent } from './auth/components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent]
+  imports: [CommonModule, MaterialModule, RouterModule, ReactiveFormsModule, FormsModule],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    PageHeaderComponent,
+    NotFoundComponent,
+    LoginFormComponent
+  ],
+  exports: [HeaderComponent, FooterComponent, PageHeaderComponent]
 })
 export class CoreModule {}
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatIconModule, MatInputModule],
-  exports: [MatIconModule, MatInputModule]
+  imports: [MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatIconModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialModule {
   constructor(iconRegistry: MatIconRegistry) {
