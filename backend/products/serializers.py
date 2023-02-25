5a9774a5581896a3b4233f68421f4e679c591486
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-02-25 20:09:29 +0200

    add small fixes

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -4,5 +4,5 @@ from rest_framework.serializers import ModelSerializer
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'amount', 'description', 'created_at', 'updated_at']
-        read_only = ['created_at']
+        fields = ["id", "name", "price", "amount", "in_stock", "description",  "created_at", "updated_at"]
+        read_only = ["created_at", "in_stock"]
