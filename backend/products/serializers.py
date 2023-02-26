217a260e753daaf502cdcf42eb35b3890a310bf8
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-02-26 17:20:45 +0200

    add new table and refactor serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,8 +1,11 @@
 from .models import Product
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer, IntegerField
 
-class ProductSerializer(ModelSerializer):
+
+class ProductSerializer(HyperlinkedModelSerializer):
+    quantity = IntegerField(source="inventory.quantity", read_only=True)
+
     class Meta:
         model = Product
-        fields = ["id", "name", "price", "amount", "in_stock", "description",  "created_at", "updated_at"]
+        fields = ["id", "url", "name", "price", "quantity", "in_stock", "description",  "created_at", "updated_at"]
         read_only = ["created_at", "in_stock"]
