4a57b139573a297943e23670b44276cfb4b6d74e
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-02-27 11:50:53 +0200

    add debug toolbar and fix n+1 problem

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('', include('drfpasswordless.urls')),
     path('', include('products.urls')),
- ]
+    ]
  
