05a96a193fbe79f2d66b15fad6f30696782ae61e
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-02 18:25:41 +0200

    article: create CreateAPIView

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleListAPIView, ArticleRetrieveAPIView
+from .views import ArticleListAPIView, ArticleRetrieveAPIView, ArticleCategoryListAPIView
 
 
 urlpatterns = [
     path("articles/", ArticleListAPIView.as_view()),
-    path("articles/<slug:slug>", ArticleRetrieveAPIView.as_view(), name="article-detai")
+    path("articles/<slug:slug>", ArticleRetrieveAPIView.as_view(), name="article-detail"),
+    path("articles/categories/", ArticleCategoryListAPIView.as_view())
 ]
