3b5c48115f52140ba4e3b83c553b903255801488
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-07 21:37:21 +0200

    pictures: start new app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
-
+from django.conf import  settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('', include('drfpasswordless.urls')),
     path('', include('products.urls')),
-    path('', include('articles.urls'))
-]
- 
+    path('', include('articles.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
