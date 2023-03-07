f8c68371c06986a4b1e7de5794448565dc870df6
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-07 23:47:33 +0200

    images: create model

--- backend/pictures/models.py
+++ backend/pictures/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import Model, CharField, ImageField, DateTimeField
+from uuid import uuid4
 
-# Create your models here.
+
+class Picture(Model):
+    title = CharField(max_length=150, default="", blank=True)
+    picture = ImageField(max_length=150, upload_to="uploaded/%Y/%m")
+    uploaded_at = DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-uploaded_at"]
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
