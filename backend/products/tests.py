5c1d4d8cb0a1c01eb19e12d13b4e75845d4c3f10
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-10 20:06:56 +0200

    testing: cover products with the tests

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -2,7 +2,8 @@ from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_401_UNAUTHORIZED
 from .filters import NamedOrderingFilter
-
+from .models import Product, ProductInventory
+from django.core.exceptions import ValidationError
 
 class FilterTestCase(APITestCase):
 
@@ -72,3 +73,115 @@ class ProductFilterTestCase(APITestCase):
         self.assertNotEqual(r.status_code, HTTP_200_OK)
         r = self.client.get(reverse("product-list"), {"min_price": 10, "max_price": 0})
         self.assertNotEqual(r.status_code, HTTP_200_OK)
+
+
+class ProductTestCase(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.data = {
+            "name": "some test name",
+            "description": "some interesting description",
+            "price": 20.10,
+            "quantity": 5,
+            "main_image": 1,
+            "images": [1,2,3]
+        }
+        cls.username = "demo"
+        cls.password = "demo1234"
+
+    def test_access_denied(self):
+        r = self.client.post(reverse("product-list"), self.data)
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+        r = self.client.patch(reverse("product-detail", pk=1), {
+            "name": "another name"
+        })
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+        r = self.client.put(reverse("product-list"), self.data)
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+        r = self.client.delete(reverse("product-detail", pk=1))
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+
+    def test_list_and_detail_view(self):
+        r = self.client.get(reverse("product-list"))
+        self.assertEqual(r.status_code, HTTP_200_OK)
+        result = r.json()["result"]
+        self.assertNotIn("images", result[0])
+        self.assertIn("main_image", result[0])
+        self.assertIn("url", result[0])
+        r = self.client.get(reverse("product-detail"), pk=1)
+        self.assertEqual(r.status_code, HTTP_200_OK)
+        result = r.json()
+        self.assertNotIn("url", result)
+        self.assertIn("images", result)
+        response_image = self.client.get(result["images"][0])
+        self.assertEqual(response_image.status_code, HTTP_200_OK)
+        response_image = self.client.get(result["main_image"])
+        self.assertEqual(response_image.status_code, HTTP_200_OK)
+
+    def test_create_product(self):
+        self.client.login(username=self.username, password=self.password)
+        r = self.client.post(reverse("product-list"), self.data)
+        self.assertEqual(r.status_code, HTTP_201_CREATED)
+        result = r.json()
+        self.assertIn("images", result)
+        self.assertNotEqual(0, len(result["images"]))
+
+
+class InventoryTestCase(APITestCase):
+
+    def setUpTestData(cls):
+        cls.username = "demo"
+        cls.password = "demo1234"
+        cls.data = {
+            "quantity": 100,
+            "sold_qty": 1000,
+        }
+
+    def test_inventory_access_denied(self):
+        r = self.client.get(reverse("product_inventory", pk=1))
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+        r = self.client.put(reverse("product_inventory", pk=1), self.data)
+        self.assertEqual(r.status_code, HTTP_401_UNAUTHORIZED)
+
+    def test_get_and_update_inventory(self):
+        self.client.login(username=self.username, password=self.password)
+        r = self.client.get(reverse("product_inventory", pk=1))
+        self.assertEqual(r.status_code, HTTP_200_OK)
+        result = r.json()
+        self.assertEqual(1, result["product_id"])
+        self.assertIn("sold_qty", result)
+        self.assertIn("quantity", result)
+        r = self.client.put(reverse("product_inventory", pk=1), self.data)
+        result = r.json()
+        for key in self.data:
+            self.assertIn(key, result)
+            self.assertEqual(result[key], self.data[key])
+
+
+class ModelsAPITestCase(APITestCase):
+
+    def test_inventory_created(self):
+        obj = Product(
+            name="abs_unique_name",
+            price=101
+        )
+        obj.save()
+        self.assertNotEqual(obj.id, None)
+        obj_inventory = ProductInventory.objects.get(product=obj)
+        self.assertNotEqual(obj_inventory, None)
+
+    def test_validation_errors(self):
+        obj = Product.objects.get(pk=1)
+        obj_inventory = ProductInventory.objects.get(product=obj)
+        with self.assertRaises(ValidationError):
+            obj.price = -100
+            obj.save()
+
+        with self.assertRaises(ValidationError):
+            obj_inventory.quantity = -100
+            obj_inventory.save()
+
+        with self.assertRaises(ValidationError):
+            obj_inventory.sold_qty = -100
+            obj_inventory.save()
