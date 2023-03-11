b1b4b63d2708b0e0c6cb28350609a344c87cefa1
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-12 00:14:53 +0200

    testing: set Up selenium for testing

--- frontend/tests/test_login.py
+++ frontend/tests/test_login.py
@@ -1,2 +1,16 @@
-def test_login():
-    assert 0 == 0
+from pytest import fixture
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+
+@fixture
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+
+
+def test_login(driver, base_url):
+    driver.get(base_url)
+    assert "Shoppe" in driver.title
+    assert 1 == 1
