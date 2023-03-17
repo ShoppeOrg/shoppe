1a52d26fd033a637c4a1e7d74fec3f17c8f1278b
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-17 15:36:53 +0200

    category-filter: add categories filter on articles

--- backend/articles/filters.py
+++ backend/articles/filters.py
@@ -1,6 +1,8 @@
-from django_filters.rest_framework import FilterSet, BooleanFilter
+from django_filters.rest_framework import FilterSet, BooleanFilter, ModelMultipleChoiceFilter
+from .models import ArticleCategory
 
 
 class ArticleFilter(FilterSet):
 
     is_published = BooleanFilter(field_name="is_published")
+    categories = ModelMultipleChoiceFilter(queryset=ArticleCategory.objects.all())
