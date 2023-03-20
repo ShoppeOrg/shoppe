323902d721dc2defcfac4ac832940e6663d7aba4
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-21 01:45:32 +0200

    product-review: add view and urlconf

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,14 +1,18 @@
+from rest_framework.generics import CreateAPIView
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ProductFilter
 from .models import Product
 from .models import ProductInventory
+from .models import Review
 from .serializers import ProductCreateSerializer
 from .serializers import ProductDetailSerializer
 from .serializers import ProductInventorySerializer
 from .serializers import ProductListSerializer
+from .serializers import ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -33,3 +37,9 @@ class ProductInventoryRetrieveUpdateAPIVIew(RetrieveUpdateAPIView):
     queryset = ProductInventory.objects.all()
     permission_classes = (IsAdminUser,)
     serializer_class = ProductInventorySerializer
+
+
+class ReviewCreateAPIView(CreateAPIView):
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ReviewSerializer
