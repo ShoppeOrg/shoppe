a4c5f9f5bbe64fc1dddc411ac5bf2eab4278b0d3
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-21 01:45:32 +0200

    product-review: add view and urlconf

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,14 +3,16 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ProductInventoryRetrieveUpdateAPIVIew
 from .views import ProductViewSet
+from .views import ReviewCreateAPIView
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
 urlpatterns = [
+    path("products/reviews/", ReviewCreateAPIView.as_view(), name="product_review"),
     path(
-        "products/<int:pk>/inventory",
+        "products/<int:pk>/inventory/",
         ProductInventoryRetrieveUpdateAPIVIew.as_view(),
         name="product_inventory",
-    )
+    ),
 ]
 urlpatterns += router.urls
