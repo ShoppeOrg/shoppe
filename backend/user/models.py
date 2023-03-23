ed2a792c2c7f599d6847d7db5abc3fc11f4a4ceb
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-23 13:16:40 +0200

    user: generate random username

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from random_username.generate import UsernameGenerator
 
 
 class User(AbstractUser):
-    username = CharField(max_length=150, default="", unique=True)
-
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = self.email
-        super().save(*args, **kwargs)
+    username = CharField(
+        max_length=150,
+        default=UsernameGenerator().generate_username(num_digits=3),
+        unique=True,
+    )
 
     @property
     def fullname(self):
