2d8d5148171578fe075a81c3bd4d77ec26cbd4fb
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-24 19:22:55 +0200

    admin-panel: list products

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -6,4 +6,7 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
-    pass
+    list_display = ["id", "name", "display_price"]
+    list_display_links = ["id", "name"]
+    search_fields = ["name", "description"]
+    ordering = ["name", "price"]
