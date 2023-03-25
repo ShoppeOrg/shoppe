af9339ae37a9d41f024e60a4c89edfb946913f0c
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-03-25 12:46:55 +0200

    admin-panel: add ability to change quantity of product

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.contrib.admin import StackedInline
 
 from .models import Product
+from .models import ProductInventory
+
+
+class InventoryInline(StackedInline):
+    model = ProductInventory
+    exclude = ["sold_qty"]
 
 
 @admin.register(Product)
@@ -10,3 +17,7 @@ class ProductAdmin(ModelAdmin):
     list_display_links = ["id", "name"]
     search_fields = ["name", "description"]
     ordering = ["name", "price"]
+    list_per_page = 10
+    inlines = [
+        InventoryInline,
+    ]
