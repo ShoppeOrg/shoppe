c4dd0b08bb07c30459473989a1e285b47fe6a749
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-04-14 23:10:39 +0300

    orders: create intermidiate table

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
+from products.models import Product
 
 from .statuses import OrderStatus
 
@@ -8,5 +10,12 @@ class Order(models.Model):
     status = models.CharField(
         max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
     )
+    goods = models.ManyToManyField(Product, through="OrderProduct")
     created_at = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
+
+
+class OrderProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
