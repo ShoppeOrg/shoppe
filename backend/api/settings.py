d1532e536659ff57a89efc0aebcda1e3d98ca2d1
oleksandrpozden <oleksandr.pozden@gmail.com>
2023-04-14 22:50:46 +0300

    orders: register app in api/settings.py and create statuses

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "products",
     "articles",
     "pictures",
+    "orders",
 ]
 
 MIDDLEWARE = [
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from .statuses import OrderStatus
+
+
+class Order(models.Model):
+    status = models.CharField(
+        max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
